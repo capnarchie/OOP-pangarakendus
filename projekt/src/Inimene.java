cb65dac8dc6a3b9310881c2a11d531ff51b979b5
Capn <uku.ilves1@gmail.com>
2021-04-08 17:47:02 +0300

    trying to resolve merge conflict

--- projekt/src/Inimene.java
+++ projekt/src/Inimene.java
@@ -1,9 +1,18 @@
 public class Inimene{
     String kasutajaNimi;
     String kontonr;
+    double kontojääk;
 
     public Inimene(String kasutajaNimi, String kontonr) {
         this.kasutajaNimi = kasutajaNimi;
         this.kontonr = kontonr;
+        this.kontojääk = suvaline();
+    }
+    public static double suvaline(){
+        String[] pangad = {"10", "22", "77"};
+        double summa =(Math.random()*(1000-0)+100);
+        summa = Math.round(summa);
+        return summa;
     }
 }
+
